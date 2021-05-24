0670d2ee595692a4bc826f5c029bc53737caa95f
Remi Pelletier <remipelletier5@gmail.com>
2021-05-23 23:28:27 -0700

    Added installer base project

---
+++ OutlinesInstaller/InstallerConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace OutlinesInstaller
+{
+    public class InstallerConfig
+    {
+        public string PackageArchiveName { get; set; } = "outlines.zip";
+        public string SourceLocation { get; set; }
+        public string InstallLocation { get; set; } = "C:\\Program Files (x86)\\outlines\\";
+        public string PackageArchiveSourcePath => Path.Combine(SourceLocation, PackageArchiveName);
+        public string PackageArchiveDestinationPath => Path.Combine(InstallLocation, PackageArchiveName);
+        public string PackageExtractionPath => InstallLocation;
+        public string CertificateName { get; set; }
+        public string CertificatePath => Path.Combine(PackageExtractionPath, CertificateName);
+        public string ApplicationName { get; set; } = "OutlinesApp.exe";
+        public string InstalledApplicationPath => Path.Combine(InstallLocation, ApplicationName);
+        public string Hotkey { get; set; } = "Ctrl+Shift+O";
+        public bool ShouldCreateDesktopShortcut { get; set; } = true;
+        public bool ShouldRegisterHotkey { get; set; } = false;
+        public bool ShouldInstallSystemApp { get; set; } = false;
+    }
+}
---
+++ OutlinesInstaller/InstallerService.cs
@@ -0,0 +1,74 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Security.Cryptography.X509Certificates;
+using IWshRuntimeLibrary;
+
+namespace OutlinesInstaller
+{
+    class InstallerService
+    {
+        private InstallerConfig Config { get; set; }
+
+        public void Install()
+        {
+            CreateInstallFolder();
+            CopyPackage();
+            ExtractPackage();
+            RegisterCertifcate();
+            if (Config.ShouldCreateDesktopShortcut)
+            {
+                CreateDesktopShortcut();
+            }
+            CleanupInstallationArtifacts();
+        }
+
+        private void CreateInstallFolder()
+        {
+            if (!Directory.Exists(Config.InstallLocation))
+            {
+                Directory.CreateDirectory(Config.InstallLocation);
+            }
+        }
+
+        private void CopyPackage()
+        {
+            System.IO.File.Copy(Config.PackageArchiveSourcePath, Config.PackageArchiveDestinationPath);
+        }
+
+        private void ExtractPackage()
+        {
+            ZipFile.ExtractToDirectory(Config.PackageArchiveDestinationPath, Config.PackageExtractionPath);
+        }
+
+        private void RegisterCertifcate()
+        {
+            X509Store store = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadWrite);
+            store.Add(new X509Certificate2(X509Certificate2.CreateFromCertFile(Config.CertificatePath)));
+            store.Close();
+        }
+
+        private void CreateDesktopShortcut()
+        {
+            WshShell shell = new WshShell();
+            object shDesktop = (object)"Desktop";
+            string shortcutPath = (string)shell.SpecialFolders.Item(ref shDesktop) + @"\Outlines.lnk";
+
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutPath);
+            shortcut.Description = "Outlines App";
+            shortcut.TargetPath = Config.InstalledApplicationPath;
+
+            if (Config.ShouldRegisterHotkey)
+            {
+                shortcut.Hotkey = Config.Hotkey;
+            }
+
+            shortcut.Save();
+        }
+
+        private void CleanupInstallationArtifacts()
+        {
+            System.IO.File.Delete(Config.PackageArchiveDestinationPath);
+        }
+    }
+}
